0e3d7a93c7d796e2872c536a9088d263478817b4
Mustafa Khalil <mustafakh1997@gmail.com>
2022-02-18 18:21:46 +0200

    Viewing all halls on the map

--- screens/MapViewer.js
+++ screens/MapViewer.js
@@ -2,12 +2,33 @@ import React from "react";
 import MapView, { Marker } from "react-native-maps";
 import { View, StyleSheet, Dimensions } from "react-native";
 
+const locations = [
+  {
+    title: "North Hall",
+    lat: 34.431093869627254,
+    lng: 35.8377506411768,
+  },
+  {
+    title: "West Hall",
+    lat: 34.15550968858545,
+    lng: 35.64338541736089,
+  },
+  {
+    title: "5 Star Hall",
+    lat: 39.92801442507861,
+    lng: 32.83767491273409,
+  },
+];
+
 const MapViewer = ({ route }) => {
   // const { lat, lng } = location;
-
+  let title;
+  let lat;
+  let lng;
   if (route.params) {
-    const { location, title } = route.params;
-    const { lat, lng } = location;
+    title = route.params.title;
+    lat = route.params.location.lat;
+    lng = route.params.location.lng;
   }
 
   const source = require("../constants/images/beautiful-photozone-with-big-wreath-decorated-with-greenery-roses-centerpiece-candles-sides-garland-hanged-trees_8353-11019.jpg");
@@ -20,19 +41,29 @@ const MapViewer = ({ route }) => {
           route.params && {
             latitude: lat,
             longitude: lng,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+            latitudeDelta: 0.03,
+            longitudeDelta: 0.03,
           }
         }
       >
-        {route.params && (
+        {locations.map((location) => {
+          const { title, lat, lng } = location;
+          return (
+            <Marker
+              pinColor="green"
+              title={title}
+              coordinate={{ latitude: lat, longitude: lng }}
+            />
+          );
+        })}
+        {/* {route.params && (
           <Marker
             pinColor="green"
             title={title}
             // image={source}
             coordinate={{ latitude: lat, longitude: lng }}
           />
-        )}
+        )} */}
       </MapView>
     </View>
   );
