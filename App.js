b4c2b87355147ec7f3efb72ebf966366a090fa9f
Mustafa Khalil <mustafakh1997@gmail.com>
2022-02-24 16:28:38 +0200

    refactored navigation code 1. created new folder to hold my two different tabNavigations 2. removed tabNavigation code from app.js to UserTabNavigation 3. another file HostTabNavigation will hold navigation for the host

--- App.js
+++ App.js
@@ -10,25 +10,17 @@ import { StatusBar } from "expo-status-bar";
 import { enableScreens } from "react-native-screens";
 import { EvilIcons, Feather, Ionicons } from "@expo/vector-icons";
 import { createStore, combineReducers, applyMiddleware } from "redux";
-import { Provider, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 
 import ReduxThunk from "redux-thunk";
 import FlashMessage from "react-native-flash-message";
-import Colors from "./constants/Colors";
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 
-import HallListScreen from "./screens/HallListScreen";
-import HallDetailScreen from "./screens/HallDetailScreen";
-import FavoriteHallsScreen from "./screens/FavoriteHallsScreen";
-import LoginScreen from "./screens/LoginScreen";
-import DefaultText from "./components/DefaultText";
-import SignupScreen from "./screens/SignupScreen";
-import MapViewer from "./screens/MapViewer";
 import hallListReducer from "./store/reducers/HallList";
 import currentLocationReducer from "./store/reducers/Location";
-import ProfileScreen from "./screens/ProfileScreen";
 import AuthReducer from "./store/reducers/Auth";
+import TabNavigator from "./navigations/UserTabNavigation";
 
 enableScreens();
 
@@ -49,9 +41,6 @@ const fetchFonts = () => {
   });
 };
 
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
 
@@ -65,203 +54,11 @@ export default function App() {
     );
   }
 
-  const HomeStack = ({ navigation, route }) => {
-    // if (route.state && route.state.index > 0) {
-    //   navigation.setOptions({
-    //     tabBarStyle: {
-    //       display: "none",
-    //     },
-    //   });
-    // } else {
-    //   navigation.setOptions({ tabBarVisible: true });
-    // }
-
-    return (
-      <Stack.Navigator
-        screenOptions={{
-          headerTitleAlign: "center",
-        }}
-      >
-        <Stack.Screen
-          name="HallList"
-          component={HallListScreen}
-          options={{
-            title: "Wedding Venues",
-          }}
-        />
-        <Stack.Screen name="HallDetail" component={HallDetailScreen} />
-      </Stack.Navigator>
-    );
-  };
-
-  const FavoriteStack = () => {
-    return (
-      <Stack.Navigator
-        screenOptions={{
-          headerTitleAlign: "center",
-        }}
-      >
-        <Stack.Screen name="MyFavorites" component={FavoriteHallsScreen} />
-        <Stack.Screen name="HallDetail" component={HallDetailScreen} />
-      </Stack.Navigator>
-    );
-  };
-
-  const MapStack = () => {
-    return (
-      <Stack.Navigator
-        screenOptions={{
-          headerTitleAlign: "center",
-        }}
-      >
-        <Stack.Screen name="Map" component={MapViewer} />
-        {/* <Stack.Screen component={HallDetailScreen} /> */}
-      </Stack.Navigator>
-    );
-  };
-
-  const AuthStack = () => {
-    const token = useSelector((state) => state.Auth.token);
-    console.log("token ", token);
-    console.log("tokennn", !token);
-    return (
-      <Stack.Navigator
-        screenOptions={{
-          headerTitleAlign: "center",
-        }}
-      >
-        {!token && (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Signup" component={SignupScreen} />
-          </>
-        )}
-        {token && (
-          <Stack.Screen
-            name="Profile"
-            component={ProfileScreen}
-            options={{
-              headerShown: false,
-            }}
-          />
-        )}
-      </Stack.Navigator>
-    );
-  };
-
   return (
     <Provider store={store}>
       <SafeAreaProvider>
         <NavigationContainer>
-          <Tab.Navigator
-            screenOptions={({ route }) => ({
-              headerShown: false,
-              tabBarHideOnKeyboard: true,
-
-              // tabBarVisibilityAnimationConfig: true,
-              headerTitleAlign: "center",
-
-              tabBarLabel: ({ focused }) => {
-                const token = useSelector((state) => state.Auth.token);
-                const fontFamily = focused ? "open-sans-bold" : "open-sans";
-                if (route.name === "Explore") {
-                  console.log("isfocused", focused);
-                  return (
-                    <Text
-                      style={{
-                        fontSize: focused ? 12 : 12.3,
-                        color: "black",
-                        fontFamily: fontFamily,
-                      }}
-                    >
-                      Explore
-                    </Text>
-                  );
-                } else if (route.name === "MapView") {
-                  return (
-                    <Text
-                      style={{
-                        fontSize: focused ? 12 : 12.3,
-                        color: "black",
-                        fontFamily: fontFamily,
-                      }}
-                    >
-                      Map
-                    </Text>
-                  );
-                } else if (route.name === "Auth") {
-                  return (
-                    <Text
-                      style={{
-                        fontSize: focused ? 12 : 12.3,
-                        color: "black",
-                        fontFamily: fontFamily,
-                      }}
-                    >
-                      {!token ? "Login" : "Profile"}
-                    </Text>
-                  );
-                } else if (route.name === "Favorites") {
-                  return (
-                    <Text
-                      style={{
-                        fontSize: focused ? 12 : 12.3,
-                        color: "black",
-                        fontFamily: fontFamily,
-                      }}
-                    >
-                      Favorites
-                    </Text>
-                  );
-                }
-              },
-              tabBarIcon: ({ focused, color, size }) => {
-                let iconName;
-                let iconColor = focused ? Colors.accentColor : "black";
-                if (route.name === "Explore") {
-                  iconName = "search";
-                } else if (route.name === "Auth") {
-                  iconName = "user";
-                } else if (route.name === "Favorites") {
-                  iconName = "heart-outline";
-                  return (
-                    <Ionicons name={iconName} size={25} color={iconColor} />
-                  );
-                } else if (route.name === "MapView") {
-                  iconName = "map-pin";
-                  return (
-                    <Feather name={iconName} size={22} color={iconColor} />
-                  );
-                }
-                return (
-                  <EvilIcons name={iconName} size={30} color={iconColor} />
-                );
-              },
-            })}
-          >
-            <Tab.Screen name="Explore" component={HomeStack} />
-            <Tab.Screen name="Favorites" component={FavoriteStack} />
-            <Tab.Screen name="MapView" component={MapStack} />
-            <Tab.Screen name="Auth" component={AuthStack} />
-          </Tab.Navigator>
-
-          {/* <Stack.Navigator
-            screenOptions={{
-              headerShown: false,
-            }}
-          >
-            <Stack.Screen name="HallList" component={TabNavigation} />
-            <Stack.Screen name="HallDetail" component={HallDetailScreen} />
-            <Stack.Screen name="Auth" component={TabNavigation} />
-            <Stack.Screen
-              name="Signup"
-              component={SignupScreen}
-              options={{
-                headerShown: true,
-              }}
-            />
-            <Stack.Screen name="MapView" component={MapViewer} />
-          </Stack.Navigator> */}
+          <TabNavigator />
         </NavigationContainer>
         <FlashMessage
           position={"center"}
---
+++ navigations/UserTabNavigation.js
@@ -0,0 +1,189 @@
+import React from "react";
+import { Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { EvilIcons, Feather, Ionicons } from "@expo/vector-icons";
+import { useSelector } from "react-redux";
+
+import Colors from "../constants/Colors";
+
+import HallListScreen from "../screens/HallListScreen";
+import HallDetailScreen from "../screens/HallDetailScreen";
+import FavoriteHallsScreen from "../screens/FavoriteHallsScreen";
+import LoginScreen from "../screens/LoginScreen";
+import DefaultText from "../components/DefaultText";
+import SignupScreen from "../screens/SignupScreen";
+import MapViewer from "../screens/MapViewer";
+import ProfileScreen from "../screens/ProfileScreen";
+import ChatScreen from "../screens/ChatScreen";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  const HomeStack = ({ navigation, route }) => {
+    return (
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: "center",
+        }}
+      >
+        <Stack.Screen
+          name="HallList"
+          component={HallListScreen}
+          options={{
+            title: "Wedding Venues",
+          }}
+        />
+        <Stack.Screen name="HallDetail" component={HallDetailScreen} />
+      </Stack.Navigator>
+    );
+  };
+
+  const FavoriteStack = () => {
+    return (
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: "center",
+        }}
+      >
+        <Stack.Screen
+          name="MyFavorites"
+          component={FavoriteHallsScreen}
+          options={{
+            title: "My Favorites",
+          }}
+        />
+        <Stack.Screen name="HallDetail" component={HallDetailScreen} />
+      </Stack.Navigator>
+    );
+  };
+
+  const MapStack = () => {
+    return (
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: "center",
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Map" component={MapViewer} />
+        {/* <Stack.Screen component={HallDetailScreen} /> */}
+      </Stack.Navigator>
+    );
+  };
+
+  const AuthStack = () => {
+    const token = useSelector((state) => state.Auth.token);
+    console.log("token ", token);
+    console.log("tokennn", !token);
+    return (
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: "center",
+        }}
+      >
+        {!token && (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Signup" component={SignupScreen} />
+          </>
+        )}
+        {token && (
+          <Stack.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+        )}
+      </Stack.Navigator>
+    );
+  };
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+
+        // tabBarVisibilityAnimationConfig: true,
+        headerTitleAlign: "center",
+
+        tabBarLabel: ({ focused }) => {
+          const token = useSelector((state) => state.Auth.token);
+          const fontFamily = focused ? "open-sans-bold" : "open-sans";
+
+          const BarText = (props) => {
+            const { label } = props;
+            return (
+              <Text
+                style={{
+                  fontSize: focused ? 11 : 11.3,
+                  color: "black",
+                  fontFamily: fontFamily,
+                }}
+              >
+                {label}
+              </Text>
+            );
+          };
+
+          let label;
+          switch (route.name) {
+            case "Explore":
+              label = "Explore";
+              break;
+            case "MapView":
+              label = "Map";
+              break;
+            case "Chats":
+              label = "Chats";
+              break;
+            case "Auth":
+              label = !token ? "Login" : "Profile";
+              break;
+            case "Favorites":
+              label = "Favorites";
+              break;
+          }
+          return <BarText label={label} />;
+        },
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          let iconColor = focused ? Colors.accentColor : "black";
+          if (route.name === "Explore") {
+            iconName = "search";
+          } else if (route.name === "Auth") {
+            iconName = "user";
+          } else if (route.name === "Favorites") {
+            iconName = "heart-outline";
+            return <Ionicons name={iconName} size={25} color={iconColor} />;
+          } else if (route.name === "MapView") {
+            iconName = "map-pin";
+            return <Feather name={iconName} size={22} color={iconColor} />;
+          } else if (route.name === "Chats") {
+            iconName = "chatbox-outline";
+            return <Ionicons name={iconName} size={22} color={iconColor} />;
+          }
+          return <EvilIcons name={iconName} size={30} color={iconColor} />;
+        },
+      })}
+    >
+      <Tab.Screen name="Explore" component={HomeStack} />
+      <Tab.Screen name="Favorites" component={FavoriteStack} />
+      <Tab.Screen name="MapView" component={MapStack} />
+      <Tab.Screen
+        name="Chats"
+        component={ChatScreen}
+        options={{
+          headerShown: true,
+        }}
+      />
+      <Tab.Screen name="Auth" component={AuthStack} />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigator;
