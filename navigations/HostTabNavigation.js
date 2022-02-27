8a715033eb093d67c195c624f482abb9ea93827e
Mustafa Khalil <mustafakh1997@gmail.com>
2022-02-27 14:59:53 +0200

    created the HostTabNavigation with only one screen the profile

--- navigations/HostTabNavigation.js
+++ navigations/HostTabNavigation.js
@@ -3,14 +3,69 @@ import { Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { EvilIcons, Feather, Ionicons } from "@expo/vector-icons";
+import HallListScreen from "./../screens/HallListScreen";
+import HallDetailScreen from "./../screens/HallDetailScreen";
+import LoginScreen from "./../screens/LoginScreen";
+import SignupScreen from "./../screens/SignupScreen";
+import ProfileScreen from "./../screens/ProfileScreen";
+import EditProfileScreen from "./../screens/EditProfileScreen";
+import { useSelector } from "react-redux";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const HostTabNavigation = () => {
+const HostTabNavigator = () => {
+  const AuthStack = () => {
+    const token = useSelector((state) => state.Auth.token);
+    console.log("token ", token);
+    console.log("tokennn", !token);
+    return (
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: "center",
+        }}
+      >
+        {!token && (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Signup" component={SignupScreen} />
+          </>
+        )}
+        {token && (
+          <>
+            <Stack.Screen
+              name="Profile"
+              component={ProfileScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Edit"
+              component={EditProfileScreen}
+              options={{
+                title: "",
+              }}
+            />
+          </>
+        )}
+      </Stack.Navigator>
+    );
+  };
+
   return (
-    <Tab.Navigator>
-      <Tab.Screen />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+
+        // tabBarVisibilityAnimationConfig: true,
+        headerTitleAlign: "center",
+      })}
+    >
+      <Tab.Screen name="Auth" component={AuthStack} />
     </Tab.Navigator>
   );
 };
+
+export default HostTabNavigator;
