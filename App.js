7a9ea998898c9e980f5d14feb89c465dfd65b65c
Mustafa Khalil <mustafakh1997@gmail.com>
2022-03-13 20:43:09 +0200

    adding new screens to the hostTabNavigation

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import "react-native-gesture-handler";
 
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Animated, Easing } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
--- navigations/HostTabNavigation.js
+++ navigations/HostTabNavigation.js
@@ -10,6 +10,12 @@ import SignupScreen from "./../screens/SignupScreen";
 import ProfileScreen from "./../screens/ProfileScreen";
 import EditProfileScreen from "./../screens/EditProfileScreen";
 import { useSelector } from "react-redux";
+import Colors from "../constants/Colors";
+import HomeScreen from "./../screens/HomeScreen";
+import ChatScreen from "../screens/ChatScreen";
+import CalendarScreen from "./../screens/CalendarScreen";
+import HostProfileScreen from "./../screens/HostProfileScreen";
+import EditHallScreen from "./../screens/EditHallScreen";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -35,7 +41,7 @@ const HostTabNavigator = () => {
           <>
             <Stack.Screen
               name="Profile"
-              component={ProfileScreen}
+              component={HostProfileScreen}
               options={{
                 headerShown: false,
               }}
@@ -47,6 +53,7 @@ const HostTabNavigator = () => {
                 title: "",
               }}
             />
+            <Stack.Screen name="EditHall" component={EditHallScreen} />
           </>
         )}
       </Stack.Navigator>
@@ -61,8 +68,72 @@ const HostTabNavigator = () => {
 
         // tabBarVisibilityAnimationConfig: true,
         headerTitleAlign: "center",
+        tabBarLabel: ({ focused }) => {
+          const token = useSelector((state) => state.Auth.token);
+          const fontFamily = focused ? "open-sans-bold" : "open-sans";
+
+          const BarText = (props) => {
+            const { label } = props;
+            return (
+              <Text
+                style={{
+                  fontSize: focused ? 11 : 11.3,
+                  color: "black",
+                  fontFamily: fontFamily,
+                }}
+              >
+                {label}
+              </Text>
+            );
+          };
+
+          let label;
+          switch (route.name) {
+            case "Home":
+              label = "Home";
+              break;
+            case "Chats":
+              label = "Chats";
+              break;
+            case "Calendar":
+              label = "Calendar";
+              break;
+            case "Auth":
+              label = !token ? "Login" : "Profile";
+              break;
+            case "Favorites":
+              label = "Favorites";
+              break;
+          }
+          return <BarText label={label} />;
+        },
+
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          let iconColor = focused ? Colors.accentColor : "black";
+          if (route.name === "Home") {
+            iconName = "home-outline";
+          } else if (route.name === "Chats") {
+            iconName = "chatbox-outline";
+          } else if (route.name === "Calendar") {
+            iconName = "calendar-outline";
+          } else if (route.name === "Auth") {
+            iconName = "user";
+            return <EvilIcons name={iconName} size={30} color={iconColor} />;
+          }
+          return <Ionicons name={iconName} size={25} color={iconColor} />;
+        },
       })}
     >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: true,
+        }}
+      />
+      <Tab.Screen name="Chats" component={ChatScreen} />
+      <Tab.Screen name="Calendar" component={CalendarScreen} />
       <Tab.Screen name="Auth" component={AuthStack} />
     </Tab.Navigator>
   );
