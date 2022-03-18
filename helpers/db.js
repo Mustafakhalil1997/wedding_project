4a05b88438860a8bc31a54a1c5a89c683a661af3
Mustafa Khalil <mustafakh1997@gmail.com>
2022-03-18 16:10:39 +0200

    moved the url to a different file(used across the application)

--- helpers/db.js
+++ helpers/db.js
@@ -1,4 +1,4 @@
-import { SQLite } from "expo-sqlite";
+import * as SQLite from "expo-sqlite";
 
 const db = SQLite.openDatabase("wedding.db");
 
@@ -10,6 +10,42 @@ export const init = () => {
         [],
         () => {
           resolve();
+        },
+        (_, err) => {
+          reject();
+        }
+      );
+    });
+  });
+  return promise;
+};
+
+export const setToken = (token) => {
+  const promise = new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "INSERT INTO Token (token) VALUES (?)",
+        [token],
+        (_, result) => {
+          resolve(result);
+        },
+        (_, err) => {
+          reject();
+        }
+      );
+    });
+  });
+  return promise;
+};
+
+export const getToken = () => {
+  const promise = new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM Token",
+        [],
+        (_, result) => {
+          resolve(result);
         },
         (_, err) => {
           reject();
---
+++ helpers/url.js
@@ -0,0 +1,1 @@
+export const URL = "http://b15d-185-101-16-102.ngrok.io";
