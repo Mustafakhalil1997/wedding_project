c90375ce838d7efcc5eb6b4ab4b426e8b99aac3c
Mustafa Khalil <mustafakh1997@gmail.com>
2022-03-20 00:25:58 +0200

    added a useEffect hook inside HallListScreen 1. this will be fired each time userInfo changes in redux store 2. then update AsyncStorage with the new data

--- screens/FavoriteHallsScreen.js
+++ screens/FavoriteHallsScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
 import HallList from "../components/HallList";
+import FavoriteHallList from "./../components/FavoriteHallList";
 
 const FavoriteHallsScreen = ({ navigation }) => {
+  return <FavoriteHallList navigation />;
   return <HallList favorite favoriteNavigation={navigation} />;
 };
 
--- screens/HallListScreen.js
+++ screens/HallListScreen.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, StyleSheet, Text, TouchableNativeFeedback } from "react-native";
 import HallList from "../components/HallList";
-import ImageSlider from "./../components/ImageSliderShow/ImageSlider";
-import { SliderBox } from "react-native-image-slider-box";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useSelector } from "react-redux";
 
 const HallListScreen = ({ navigation }) => {
   const source = require("../constants/images/beautiful-photozone-with-big-wreath-decorated-with-greenery-roses-centerpiece-candles-sides-garland-hanged-trees_8353-11019.jpg");
   const source1 = require("../constants/images/illustration-light-garland-transparent-background_257584-674.jpg");
+
+  const userInfo = useSelector((state) => state.Auth.userInfo);
+
+  // still can't decide the right place for calling this method, maybe app.js would be better
+  // this doesn't work in app.js as the file only runs once when the app is loaded
+  useEffect(() => {
+    console.log("hereee");
+    const setStorage = async () => {
+      await AsyncStorage.getItem("@token", async (err, val) => {
+        const jsonValue = JSON.parse(val);
+        const newValue = { ...jsonValue, userInfo };
+        const newJsonObject = JSON.stringify(newValue);
+        await AsyncStorage.setItem("@token", newJsonObject);
+      });
+    };
+    if (userInfo) setStorage();
+  }, [userInfo]);
 
   return (
     <View style={styles.screenContainer}>
