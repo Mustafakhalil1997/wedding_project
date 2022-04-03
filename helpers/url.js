7c481aa6b0839065d29dccc42358f6938be0a2b7
Mustafa Khalil <mustafakh1997@gmail.com>
2022-04-04 01:52:09 +0300

    using ngrok to test the backend locally

--- helpers/url.js
+++ helpers/url.js
@@ -1,1 +1,2 @@
-export const URL = "https://wedding-project-backend.herokuapp.com";
+// export const URL = "https://wedding-project-backend.herokuapp.com";
+export const URL = "http://42f1-185-101-16-98.ngrok.io";
