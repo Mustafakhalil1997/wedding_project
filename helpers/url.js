85ee39e90fb18e04d51714ffb02fd825f66b2fe6
Mustafa Khalil <mustafakh1997@gmail.com>
2022-04-15 06:45:04 +0300

    started working on a sliding form for host setup

--- helpers/url.js
+++ helpers/url.js
@@ -1,2 +1,2 @@
 // export const URL = "https://wedding-project-backend.herokuapp.com";
-export const URL = "http://af90-185-101-16-99.ngrok.io";
+export const URL = "http://a1c6-185-101-16-100.ngrok.io";
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,26 @@
-import React, { useState } from "react";
-import { View, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import React, { useState, useRef } from "react";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 import DefaultText from "../components/DefaultText";
 import { useSelector } from "react-redux";
+import * as ImagePicker from "expo-image-picker";
 
-// import * as ImagePicker from "expo-image-picker";
-// import ImagePicker from "react-native-image-crop-picker";
+const { width } = Dimensions.get("window");
 
 const HomeScreen = (props) => {
   const { navigation } = props;
 
   const hallInfo = useSelector((state) => state.Auth.hallInfo);
 
-  console.log("hallInfoo ", hallInfo);
-
   const [profileImagePicked, setProfileImagePicked] = useState();
+  const [pageNum, setPageNum] = useState(1);
+
+  const scrollRef = useRef();
 
   const goToName = () => {
     navigation.navigate({
@@ -21,33 +28,54 @@ const HomeScreen = (props) => {
     });
   };
 
-  const pickImages = async () => {
-    // const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    // if (status !== "granted") {
-    //   Alert.alert(
-    //     "Permission denied",
-    //     "Allow access to camera in your settings"
-    //   );
-    // }
-    // let result = await ImagePicker.launchImageLibraryAsync({
-    //   mediaTypes: ImagePicker.MediaTypeOptions.All,
-    //   allowsEditing: true,
-    //   aspect: [4, 4],
-    //   // base64: true,
-    //   // quality: 1,
-    //   allowsMultipleSelection: true,
-    // });
-    // console.log("result ", result);
-    // if (!result.cancelled) {
-    //   // const imageSize = result.base64.length * (3 / 4) - 2;
-    //   // console.log("imageSize ", imageSize);
-    //   // if (imageSize > 1000000) {
-    //   //   console.log("Big Image");
-    //   // }
-    //   setProfileImagePicked(result.uri);
-    //   // setHasUnsavedChanges(true);
-    // }
+  const pickImage = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert(
+        "Permission denied",
+        "Allow access to camera in your settings"
+      );
+    }
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 4],
+      // base64: true,
+      quality: 1,
+    });
+    console.log("result ", result);
+    if (!result.cancelled) {
+      // const imageSize = result.base64.length * (3 / 4) - 2;
+      // console.log("imageSize ", imageSize);
+      // if (imageSize > 1000000) {
+      //   console.log("Big Image");
+      // }
+      setProfileImagePicked(result.uri);
+      setHasUnsavedChanges(true);
+    }
   };
+
+  const goToNext = () => {
+    const newPageNum = pageNum + 1;
+    setPageNum(newPageNum);
+    scrollRef.current.scrollTo({
+      x: 360 * (newPageNum - 1),
+      animated: true,
+    });
+  };
+
+  const goToPrevious = () => {
+    const newPageNum = pageNum - 1;
+    setPageNum(newPageNum);
+    scrollRef.current.scrollTo({
+      x: 360 * (newPageNum - 1),
+      animated: true,
+    });
+  };
+
+  if (profileImagePicked) {
+  }
 
   return (
     <ScrollView contentContainerStyle={styles.screenContainer}>
@@ -59,7 +87,7 @@ const HomeScreen = (props) => {
             </DefaultText>
           </View>
         </TouchableOpacity>
-        <TouchableOpacity onPress={pickImages}>
+        <TouchableOpacity onPress={pickImage}>
           <View style={styles.buttonTextStyle}>
             <DefaultText styles={{ fontFamily: "open-sans-bold" }}>
               Pick images
@@ -71,6 +99,61 @@ const HomeScreen = (props) => {
           onChange={(num, onSubmit) => {}}
           callback={(callback) => {}}
         /> */}
+      </View>
+      <View style={{ height: 250 }}>
+        <ScrollView
+          ref={scrollRef}
+          horizontal
+          pagingEnabled
+          scrollEnabled={false}
+          showsHorizontalScrollIndicator={false}
+          decelerationRate={0.1}
+        >
+          <View style={{ width: width, backgroundColor: "green" }}>
+            <DefaultText>hello</DefaultText>
+          </View>
+          <View style={{ width: width, backgroundColor: "pink" }}>
+            <DefaultText>Heyy</DefaultText>
+          </View>
+        </ScrollView>
+      </View>
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <TouchableOpacity
+          onPress={goToPrevious}
+          disabled={pageNum === 1 ? true : false}
+        >
+          <View
+            style={{
+              backgroundColor: "red",
+              opacity: pageNum === 1 ? 0.4 : 1,
+              alignSelf: "flex-end",
+              margin: 10,
+              padding: 10,
+              paddingHorizontal: 20,
+              borderRadius: 10,
+            }}
+          >
+            <DefaultText>Previous</DefaultText>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={goToNext}
+          disabled={pageNum === 2 ? true : false}
+        >
+          <View
+            style={{
+              backgroundColor: "red",
+              opacity: pageNum === 2 ? 0.4 : 1,
+              alignSelf: "flex-end",
+              margin: 10,
+              padding: 10,
+              paddingHorizontal: 20,
+              borderRadius: 10,
+            }}
+          >
+            <DefaultText>Next</DefaultText>
+          </View>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
