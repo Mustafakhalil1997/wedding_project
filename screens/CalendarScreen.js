77b502b060779046cae492300a50006026cabb45
Mustafa Khalil <mustafakh1997@gmail.com>
2022-05-10 00:33:08 +0300

    calendar now shows an agenda, to be filled with details later

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,12 +1,112 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { useSelector } from "react-redux";
 import DefaultText from "../components/DefaultText";
-import { Calendar } from "react-native-calendars";
+import {
+  Calendar,
+  Agenda,
+  DateData,
+  AgendaEntry,
+  AgendaSchedule,
+} from "react-native-calendars";
+
+const testIDs = {
+  menu: {
+    CONTAINER: "menu",
+    CALENDARS: "calendars_btn",
+    CALENDAR_LIST: "calendar_list_btn",
+    HORIZONTAL_LIST: "horizontal_list_btn",
+    AGENDA: "agenda_btn",
+    EXPANDABLE_CALENDAR: "expandable_calendar_btn",
+    WEEK_CALENDAR: "week_calendar_btn",
+    TIMELINE_CALENDAR: "timeline_calendar_btn",
+  },
+  calendars: {
+    CONTAINER: "calendars",
+    FIRST: "first_calendar",
+    LAST: "last_calendar",
+  },
+  calendarList: { CONTAINER: "calendarList" },
+  horizontalList: { CONTAINER: "horizontalList" },
+  agenda: {
+    CONTAINER: "agenda",
+    ITEM: "item",
+  },
+  expandableCalendar: { CONTAINER: "expandableCalendar" },
+  weekCalendar: { CONTAINER: "weekCalendar" },
+};
 
 const CalendarScreen = () => {
+  const [items, setItems] = useState({});
+
+  const hallInfo = useSelector((state) => state.Auth.hallInfo);
+  console.log("hallInfo ", hallInfo);
+
+  // next populate calendar with info about users
+
+  const renderEmptyDate = () => {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is empty date!</Text>
+      </View>
+    );
+  };
+
+  const rowHasChanged = (r1, r2) => {
+    return r1.name !== r2.name;
+  };
+
+  const timeToString = (time) => {
+    const date = new Date(time);
+    return date.toISOString().split("T")[0];
+  };
+
+  const renderItem = (reservation, isFirst) => {
+    const fontSize = isFirst ? 16 : 14;
+    const color = isFirst ? "black" : "#43515c";
+
+    return (
+      <TouchableOpacity
+        testID={testIDs.agenda.ITEM}
+        style={[styles.item, { height: reservation.height }]}
+        onPress={() => Alert.alert(reservation.name)}
+      >
+        <Text style={{ fontSize, color }}>{reservation.name}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+
+        if (!items[strTime]) {
+          items[strTime] = [];
+
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: "Item for " + strTime + " #" + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+              day: strTime,
+            });
+          }
+        }
+      }
+
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 1000);
+  };
+
   return (
     <View style={styles.screenContainer}>
-      <Calendar
+      {/* <Calendar
         style={{ height: 350 }}
         onDayPress={(day) => {
           console.log("selected day", day.dateString);
@@ -32,6 +132,31 @@ const CalendarScreen = () => {
         }}
         // Enable the option to swipe between months. Default = false
         enableSwipeMonths={true}
+      /> */}
+      <Agenda
+        // testID={testIDs.agenda.CONTAINER}
+        items={items}
+        loadItemsForMonth={loadItems}
+        // selected={"2022-05-16"}
+        renderItem={renderItem}
+        renderEmptyDate={renderEmptyDate}
+        rowHasChanged={rowHasChanged}
+        showClosingKnob={true}
+        // markingType={'period'}
+        // markedDates={{
+        //    '2017-05-08': {textColor: '#43515c'},
+        //    '2017-05-09': {textColor: '#43515c'},
+        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+        //    '2017-05-21': {startingDay: true, color: 'blue'},
+        //    '2017-05-22': {endingDay: true, color: 'gray'},
+        //    '2017-05-24': {startingDay: true, color: 'gray'},
+        //    '2017-05-25': {color: 'gray'},
+        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+        // monthFormat={'yyyy'}
+        // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+        // hideExtraDays={false}
+        // showOnlySelectedDayItems
       />
     </View>
   );
@@ -42,6 +167,19 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     flex: 1,
     backgroundColor: "white",
+  },
+  item: {
+    backgroundColor: "white",
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17,
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30,
   },
 });
 
