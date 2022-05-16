14116936de6a5a8c32745f9af761863b323942aa
Mustafa Khalil <mustafakh1997@gmail.com>
2022-05-16 14:51:49 +0300

    built chatScreenand filled with dummy data

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,22 +1,120 @@
-import React from "react";
+import {
+  FontAwesome,
+  Ionicons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
+import React, { useState, useCallback, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
+import { GiftedChat, Bubble, Send } from "react-native-gifted-chat";
 import DefaultText from "../components/DefaultText";
 
 const ChatScreen = (props) => {
   const { route, navigation } = props;
 
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 4,
+        text: "Thanks Jenny, sure why not",
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 3,
+        text: "You look like a handsome man, I want to get to know you",
+        createdAt: new Date(),
+        user: {
+          _id: 3,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: "Hey jenny",
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
+
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#2e64e5",
+          },
+        }}
+        textStyle={{
+          right: {
+            color: "#fff",
+          },
+        }}
+      />
+    );
+  };
+
+  const renderSend = (props) => {
+    return (
+      <Send {...props}>
+        <View style={{ marginBottom: 4, marginRight: 5 }}>
+          <MaterialCommunityIcons
+            name="send-circle"
+            size={42}
+            color="#2e64e5"
+          />
+        </View>
+      </Send>
+    );
+  };
+
+  const scrollToBottomComponent = () => {
+    return <FontAwesome name="angle-double-down" size={22} color="#333" />;
+  };
+
   return (
     <View style={styles.screenContainer}>
-      <DefaultText
-        styles={{
-          fontSize: 18,
-          fontFamily: "open-sans-bold",
-          marginBottom: 10,
+      <GiftedChat
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
         }}
-      >
-        No new messages
-      </DefaultText>
-      <DefaultText>When you have a message, it will appear here</DefaultText>
+        renderBubble={renderBubble}
+        alwaysShowSend
+        renderSend={renderSend}
+        scrollToBottom
+        scrollToBottomComponent={scrollToBottomComponent}
+      />
     </View>
   );
 };
