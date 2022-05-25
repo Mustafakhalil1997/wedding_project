882215c2aea9131f0b0fcb6ec0280f0f9ab78983
Mustafa Khalil <mustafakh1997@gmail.com>
2022-05-25 19:17:36 +0300

    updated url

--- helpers/url.js
+++ helpers/url.js
@@ -1,2 +1,2 @@
 // export const URL = "https://wedding-project-backend.herokuapp.com";
-export const URL = "http://dce3-185-101-16-100.ngrok.io";
+export const URL = "http://5122-185-81-141-158.ngrok.io";
