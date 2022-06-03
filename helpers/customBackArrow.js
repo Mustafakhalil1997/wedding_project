c94e06018d4f05d3ec0720b139d84a3f28fda720
Mustafa Khalil <mustafakh1997@gmail.com>
2022-06-03 14:01:13 +0300

    added an optional onPress prop

--- helpers/customBackArrow.js
+++ helpers/customBackArrow.js
@@ -2,7 +2,7 @@ import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/HeaderButton";
 
 const customBackArrow = (props) => {
-  const { navigation, isSubmitting, style } = props;
+  const { navigation, isSubmitting, style, onPress } = props;
 
   return navigation.setOptions({
     headerLeft: () => (
@@ -10,11 +10,15 @@ const customBackArrow = (props) => {
         <Item
           title="Save"
           iconName="arrow-back"
-          onPress={() => {
-            if (!isSubmitting) {
-              navigation.goBack(null);
-            }
-          }}
+          onPress={
+            onPress
+              ? onPress
+              : () => {
+                  if (!isSubmitting) {
+                    navigation.goBack(null);
+                  }
+                }
+          }
           style={[{ opacity: isSubmitting ? 0.3 : 1 }, style]}
         />
       </HeaderButtons>
